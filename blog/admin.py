15b012dec366c70e48193a01b75eadaeb77c0067
Kasiet2001 <mursidinovakasiet@gmail.com>
2024-08-16 22:40:21 +0600

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from . import models
+
+
+class RecipeInline(admin.StackedInline):
+    model = models.Recipe
+    extra = 1
+
+
+@admin.register(models.Category)
+class CategoryAdmin(MPTTModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ['title', 'category', 'author', 'created_at', 'id']
+    inlines = [RecipeInline]
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'prep_time', 'cook_time', 'post']
+
+
+
+
+admin.site.register(models.Comment)
---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.home),
+    path('<slug:slug>/', views.PostListView.as_view(), name='post_list'),
+    path('<slug:slug>/<slug:post_slug>/', views.PostDetailView.as_view(), name='post_single'),
+
+]
